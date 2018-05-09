29c4b7d18f3210f506c86fa8bf19041bff3db464
Kyle Cooley <kyleacooley@gmail.com>
2018-05-09 15:43:49 -0400

    Clean up RakNet build, get rid of terra

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -13,10 +13,4 @@ cd ~/
 git clone https://github.com/TES3MP/RakNet
 cd RakNet
 cmake . -DRAKNET_ENABLE_DLL=OFF -DRAKNET_ENABLE_SAMPLES=OFF -DCMAKE_BUILD_TYPE=Release
-mkdir ./lib
-make -j3 install
-cp ./Lib/RakNetLibStatic/libRakNetLibStatic.a ./lib
-cd ..
-
-wget https://github.com/zdevito/terra/releases/download/release-2016-03-25/terra-Linux-x86_64-332a506.zip
-unzip terra-Linux-x86_64-332a506.zip
+make -j3
