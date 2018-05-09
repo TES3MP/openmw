63ecc9fe292cfd77cf38c76f6366d17deaa116cc
Kyle Cooley <kyleacooley@gmail.com>
2018-05-09 15:09:29 -0400

    Get rid of incomplete osx fragments and setup gcc/clang compilers

--- CI/before_install.linux.sh
+++ CI/before_install.linux.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-
-sudo ln -s /usr/bin/clang-3.6 /usr/local/bin/clang
-sudo ln -s /usr/bin/clang++-3.6 /usr/local/bin/clang++
 
 # build libgtest & libgtest_main
 sudo mkdir /usr/src/gtest/build
