993078a27d17de4d9b3329b6292d7b51290838e0
xefinity <75137480+xefinity@users.noreply.github.com>
2022-03-08 22:49:29 +0300

    fix https://github.com/TES3MP/TES3MP/issues/604

--- components/openmw-mp/Packets/Player/PacketPlayerJournal.cpp
+++ components/openmw-mp/Packets/Player/PacketPlayerJournal.cpp
@@ -21,6 +21,16 @@ void PacketPlayerJournal::Packet(RakNet::BitStream *newBitstream, bool send)
 
     if (!send)
     {
+        // Sanity check
+        /* 2nd value: there is no fixed length, but the compressed header (1b+4b) is 100% bigger than 1 byte. I'm just lazy to do maths.*/
+        const static int minEntrySize = sizeof(JournalItem::type) + 1 + sizeof(JournalItem::index);
+        int estimatedMax = bs.GetNumberOfUnreadBits() / (minEntrySize * 8);
+        if (count > estimatedMax)
+        {
+            LOG_MESSAGE(TimedLog::LOG_ERROR, "[PacketPlayerJournal] Too big journal changes count: %d (est. max: %d)", count, estimatedMax);
+            packetValid = false;
+            return;
+        }
         player->journalChanges.clear();
         player->journalChanges.resize(count);
     }
