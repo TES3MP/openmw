ac8cfaeb6d15ac37dd4656a97e3fc0975a86f2a4
xefinity <75137480+xefinity@users.noreply.github.com>
2022-03-08 22:53:07 +0300

    fix https://github.com/TES3MP/TES3MP/issues/604

--- components/openmw-mp/Packets/Player/PacketPlayerSpellbook.cpp
+++ components/openmw-mp/Packets/Player/PacketPlayerSpellbook.cpp
@@ -23,6 +23,15 @@ void PacketPlayerSpellbook::Packet(RakNet::BitStream *newBitstream, bool send)
 
     if (!send)
     {
+        // Sanity check
+        const static int minEntrySize = 1; // there is no fixed length, but the compressed header (1b+4b) is 100% bigger than 1 byte. i'm just lazy to do maths.
+        int estimatedMax = bs.GetNumberOfUnreadBits() / (minEntrySize * 8);
+        if (count > estimatedMax)
+        {
+            LOG_MESSAGE(TimedLog::LOG_ERROR, "[PacketPlayerSpellbook] Too big spellbook changes count: %d (est. max: %d)", count, estimatedMax);
+            packetValid = false;
+            return;
+        }
         player->spellbookChanges.spells.clear();
         player->spellbookChanges.spells.resize(count);
     }
