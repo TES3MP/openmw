9f779371b3be0ca6c87ad5d2ca6024f07dbd5722
xefinity <75137480+xefinity@users.noreply.github.com>
2022-03-08 22:38:09 +0300

    fix https://github.com/TES3MP/TES3MP/issues/604

--- components/openmw-mp/Packets/Player/PacketPlayerEquipment.cpp
+++ components/openmw-mp/Packets/Player/PacketPlayerEquipment.cpp
@@ -32,6 +32,17 @@ void PacketPlayerEquipment::Packet(RakNet::BitStream *newBitstream, bool send)
 
         if (!send)
         {
+            // Sanity check
+            const static int minEntrySize = sizeof(int) + 1 + sizeof(Item::count) + sizeof(Item::charge) + sizeof(Item::enchantmentCharge);
+            /* Second value: there is no fixed length,
+                but the compressed header (1b+4b) is 100% bigger than 1 byte. I'm just lazy to do maths.*/
+            int estimatedMax = bs.GetNumberOfUnreadBits() / (minEntrySize * 8);
+            if (count > estimatedMax)
+            {
+                LOG_MESSAGE(TimedLog::LOG_ERROR, "[PacketPlayerEquipment] Too big equipment changes count: %d (est. max: %d)", count, estimatedMax);
+                packetValid = false;
+                return;
+            }
             player->equipmentIndexChanges.clear();
             player->equipmentIndexChanges.resize(count);
         }
