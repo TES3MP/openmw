ea168c5a4766b570c4a7978d2455b4029b57ff83
Dave Corley <smokydave@icloud.com>
2023-10-05 19:43:09 -0500

    No just stupid windows things

--- cmake/FindRakNet.cmake
+++ cmake/FindRakNet.cmake
@@ -20,35 +20,29 @@ MESSAGE(STATUS ${RakNet_INCLUDES})
 MESSAGE(STATUS ${RakNet_LIBRARY_RELEASE})
 MESSAGE(STATUS ${RakNet_LIBRARY_DEBUG})
 
-IF(RakNet_INCLUDES)
-    SET(RakNet_FOUND TRUE)
-ENDIF(RakNet_INCLUDES)
+IF(RakNet_FIND_REQUIRED)
+	MESSAGE(STATUS "Could not find RakNet, building from local copy")
 
-IF(NOT RakNet_FOUND)
-  IF(RakNet_FIND_REQUIRED)
-    MESSAGE(STATUS "Could not find RakNet, building from local copy")
+	find_package(Git QUIET)
 
-    find_package(Git QUIET)
-
-    if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
-      # Update submodules as needed
-      option(GIT_SUBMODULE "Check submodules during build" ON)
-      if(GIT_SUBMODULE)
-        message(STATUS "Submodule update")
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init
-                        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/extern/raknet
-                        RESULT_VARIABLE GIT_SUBMOD_RESULT)
-        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+	if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
+	# Update submodules as needed
+	  option(GIT_SUBMODULE "Check submodules during build" ON)
+	  if(GIT_SUBMODULE)
+		message(STATUS "Submodule update")
+		execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init
+		WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/extern/raknet
+		RESULT_VARIABLE GIT_SUBMOD_RESULT)
+		if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+			message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+		endif()
+	   endif()
         endif()
-      endif()
-    endif()
 
-    if(NOT EXISTS "${PROJECT_SOURCE_DIR}/extern/raknet/CMakeLists.txt")
-      message(FATAL_ERROR "The submodules were not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
-    endif()
-  ENDIF(RakNet_FIND_REQUIRED)
-ENDIF(NOT RakNet_FOUND)
+	if(NOT EXISTS "${PROJECT_SOURCE_DIR}/extern/raknet/CMakeLists.txt")
+	       message(FATAL_ERROR "The submodules were not downloaded! GIT_SUBMODULE was turned off or failed. Please update submodules and try again.")
+	endif()
+ENDIF(RakNet_FIND_REQUIRED)
 
 add_subdirectory(extern/raknet)
 
